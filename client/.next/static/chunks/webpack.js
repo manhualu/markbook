926fb634d834d06762255a5c4fcf7259f6a16dc1
manhualu <manhua.lu64@gmail.com>
2023-01-05 01:49:30 +1100

    Made navbar prettier with padding and flex

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0a74d0916a8a5719"; }
+/******/ 		__webpack_require__.h = function() { return "db5498dc34568e15"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -7,12 +7,13 @@ const Navbar = () => {
   return (
     <nav className="border-black-200 bg-orange-300 px-2 py-2.5 sm:px-4">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
-        <Link href="/" className="flex items-center">
-          <AiOutlineHome size={20} />
-          <span className="self-center text-xl font-bold">Markbook</span>
+        <Link href="/" className="flex space-x-2">
+          <AiOutlineHome size={30} />
+          <span className="self-center text-2xl font-bold">Markbook</span>
         </Link>
-        <button className="flex rounded border border-black bg-transparent">
-          My Profile <CgProfile size={20} />
+        <button className="p-1 flex items-center space-x-1 rounded border border-black bg-transparent">
+          <span>My Profile</span>
+          <CgProfile size={30} />
         </button>
       </div>
     </nav>
